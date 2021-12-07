010c9195ccedceeebe2fbcde29e1fc628dcc16c8
Carlos Figueiredo <carlos.figueiredo87@gmail.com>
2021-12-07 02:40:19 -0500

    Create User and add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,26 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     CodeExercise.Repo.insert!(%CodeExercise.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+require Logger
+
+alias CodeExercise.{Repo, User}
+
+# generate 1_000_000 users with default values in batches
+total = 1_000_000
+batch_size = 10_000
+batch_count = div(total, batch_size)
+
+1..total
+|> Stream.chunk_every(batch_size)
+|> Stream.map(fn [batch_start | _] = chunk ->
+  batch_number = div(batch_start, batch_size)
+  dt = NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
+
+  Logger.debug("[INSERT USER] Inserting batch ##{batch_number} of #{batch_count}")
+
+  entries =
+    Enum.map(chunk, fn _idx ->
+      %User{points: 0, inserted_at: dt, updated_at: dt}
+      |> Map.take([:points, :inserted_at, :updated_at])
+    end)
+
+  Repo.insert_all(User, entries)
+end)
+|> Enum.count()
